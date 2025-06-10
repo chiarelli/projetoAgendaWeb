c662863509d76b480cea94188287237d699b8a81
Raphael Mathias <oi.sucesso@gmail.com>
2025-06-09 23:17:06 -0300

    Created null 'categoria' and 'tarefa' representation

--- src/app/dtos/interfaces.ts
+++ src/app/dtos/interfaces.ts
@@ -11,3 +11,28 @@ export interface Tarefa {
   data: string; // formato ISO ou 'yyyy-MM-dd'
   hora: string; // formato 'HH:mm' ou similar
 }
+
+export interface TarefaUpdate {
+  titulo: string;
+  data: string; // formato ISO ou 'yyyy-MM-dd'
+  hora: string; // formato 'HH:mm' ou similar
+  finalizado: boolean;
+  categoria_id: string;
+}
+
+class CategoriaNull implements Categoria {
+  id = '';
+  nome = '';
+}
+
+class TarefaNull implements Tarefa {
+  id = '';
+  titulo = '';
+  finalizado = false;
+  categoria = new CategoriaNull();
+  data = '';
+  hora = '';
+}
+
+export const TAREFA_NULL = Object.freeze(new TarefaNull());
+export const CATEGORIA_NULL = Object.freeze(new CategoriaNull());
